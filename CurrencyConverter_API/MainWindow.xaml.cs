6f9df742e7589e341fa2191cb73cf6eee7f4f2d3
GovindD24 <Govinddeepesh@gmail.com>
2024-06-01 16:41:12 +0530

    currency converter with api

---
+++ CurrencyConverter_API/MainWindow.xaml.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Newtonsoft.Json;
+
+namespace CurrencyConverter_API
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Root val = new Root();
+
+        public class Root
+        {
+            public Rate rates { get; set; }
+            public long timestamp;
+            public string license;
+        }
+
+        public class Rate
+        {
+            public double INR {  get; set; }
+            public double JPY {  get; set; }
+            public double USD {  get; set; }
+            public double NZD {  get; set; }
+            public double EUR {  get; set; }
+            public double CAD {  get; set; }
+            public double ISK {  get; set; }
+            public double PHP {  get; set; }
+            public double DKK {  get; set; }
+            public double CZK {  get; set; }
+            public double AED { get; set; }
+            public double AFN { get; set; }
+            public double ALL { get; set; }
+            public double AMD { get; set; }
+            public double ANG {  get; set; }
+            public double AOA { get; set; }
+            public double ARS { get; set; }
+            public double AUD { get; set; }
+            public double AWG { get; set; }
+            public double AZN { get; set; }
+            public double BAM { get; set; }
+            public double BBD { get; set; }
+            public double BDT { get; set; }
+            public double  BGN { get; set; }
+            public double BHD { get; set; }
+            public double BIF { get; set; }
+   
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ClearControls();
+            BindCurrency();
+        }
+        private async void GetValue()
+        {
+            val = await GetData<Root>("https://openexchangerates.org/api/latest.json?app_id=5d181bafbd3f43deb401e9207784de9a");
+            BindCurrency();
+        }
+
+        public static async Task<Root> GetData<T>(string url)
+        {
+            var myRoot = new Root();
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    client.Timeout = TimeSpan.FromMinutes(1);
+                    HttpResponseMessage response = await client.GetAsync(url);
+                    if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                    {
+                        var ResponseString = await response.Content.ReadAsStringAsync();
+                        var ResponseObject = JsonConvert.DeserializeObject<Root>(ResponseString);
+                        MessageBox.Show("Rates: " + ResponseObject.rates, "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+                        return ResponseObject;
+                    }
+                    return myRoot;
+                }
+            }
+            catch
+            {
+                return myRoot;
+            }
+        }
+
+        #region Bind Currency From and To Combobox
+        private void BindCurrency()
+
+        {
+            //Add rows in the Datatable with text and value
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Text");
+            dt.Columns.Add("Value");
+            //Add rows in the datatable with text
+            dt.Rows.Add("--SELECT--", 0);
+            dt.Rows.Add("USD", val.rates.USD);
+            dt.Rows.Add("EUR", val.rates.EUR);
+            dt.Rows.Add("PHP", val.rates.PHP);
+            dt.Rows.Add("DKK", val.rates.DKK);
+            dt.Rows.Add("CAD", val.rates.CAD);
+            dt.Rows.Add("JPY", val.rates.JPY);
+            dt.Rows.Add("NZD", val.rates.NZD);
+            dt.Rows.Add("CZK", val.rates.CZK);
+            dt.Rows.Add("ISK", val.rates.ISK);
+
+            cmbFromCurrency.ItemsSource = dt.DefaultView;
+            cmbFromCurrency.DisplayMemberPath = "Text";
+            cmbFromCurrency.SelectedValuePath = "Value";
+            cmbFromCurrency.SelectedIndex = 0;
+
+            cmbToCurrency.ItemsSource = dt.DefaultView;
+            cmbToCurrency.DisplayMemberPath = "Text";
+            cmbToCurrency.SelectedValuePath = "Value";
+            cmbToCurrency.SelectedIndex = 0;
+        }
+        #endregion
+
+        #region Button Click Event
+
+        private void Convert_Click(object sender, RoutedEventArgs e)
+        {
+            double ConvertedValue;
+            if (txtCurrency.Text == null || txtCurrency.Text.Trim() == "")
+            {
+                MessageBox.Show("Please Enter Currency", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+                txtCurrency.Focus();
+                return;
+            }
+            else if (cmbFromCurrency.SelectedValue == null || cmbFromCurrency.SelectedIndex == 0)
+            {
+                MessageBox.Show("Please Select Currency From", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+                cmbFromCurrency.Focus();
+                return;
+            }
+            else if (cmbToCurrency.SelectedValue == null || cmbToCurrency.SelectedIndex == 0)
+            {
+                MessageBox.Show("Please Select Currency To", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+                cmbToCurrency.Focus();
+                return;
+            }
+
+            //Check if From and To Combobox selected values are same
+            if (cmbFromCurrency.Text == cmbToCurrency.Text)
+            {
+                ConvertedValue = double.Parse(txtCurrency.Text);
+                lblCurrency.Content = cmbToCurrency.Text + " " + ConvertedValue.ToString("N3");
+            }
+            else
+            {
+                ConvertedValue = (double.Parse(cmbToCurrency.SelectedValue.ToString()) * double.Parse(txtCurrency.Text)) / double.Parse(cmbFromCurrency.SelectedValue.ToString());
+                lblCurrency.Content = cmbToCurrency.Text + " " + ConvertedValue.ToString("N3");
+            }
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            ClearControls();
+        }
+        #endregion
+
+        #region Extra Events
+
+        private void ClearControls()
+        {
+            txtCurrency.Text = string.Empty;
+            if (cmbFromCurrency.Items.Count > 0)
+                cmbFromCurrency.SelectedIndex = 0;
+            if (cmbToCurrency.Items.Count > 0)
+                cmbToCurrency.SelectedIndex = 0;
+            lblCurrency.Content = "";
+            txtCurrency.Focus();
+        }
+
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9.]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+        #endregion
+    }
+}
